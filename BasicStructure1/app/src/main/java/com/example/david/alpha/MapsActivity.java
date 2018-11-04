b1e77e90ae918466c0f132a65c9906b1d2231045
Josh Morgan <jmorgan@g.hmc.edu>
2018-11-04 14:02:11 -0800

    Hey hey, maps are working in this one

--- BasicStructure1/app/src/main/java/com/example/david/alpha/MapsActivity.java
+++ BasicStructure1/app/src/main/java/com/example/david/alpha/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng shan = new LatLng(34.106413, -117.710970);
+        mMap.addMarker(new MarkerOptions().position(shan).title("Marker on Shan"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(shan));
     }
 }
