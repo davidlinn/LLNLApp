a3c114a9ae27c29e1290f3779e4b9ea929bfbf22
Richard Harris <rkharris12@gmail.com>
2019-02-27 21:03:59 -0800

    completed working first draft of puzzle answer submission

--- BasicStructure1/app/src/main/java/com/example/david/alpha/PuzzleInputActivity.java
+++ BasicStructure1/app/src/main/java/com/example/david/alpha/PuzzleInputActivity.java
@@ -1,4 +1,5 @@
 package com.example.david.alpha;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +18,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import static com.example.david.alpha.ActiveHoursActivity.userData;
+
 /*
     Puzzle input activity:  Allows user to input puzzle answers.  Initiates request to google sheets
     database containing puzzle answers.  Compares input to correct answer and resturns if the user
@@ -25,10 +28,16 @@ import org.json.JSONObject;
 
 public class PuzzleInputActivity extends AppCompatActivity {
 
+    public String sharedPrefFile = "com.example.david.alpha";
+    private static String PuzzleID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_input);
+
+        userData = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        PuzzleID = userData.getString(GlobalParams.PUZZLEID_KEY, "PUZZLEA");
     }
 
     public void submitInput(View view) {
@@ -38,10 +47,12 @@ public class PuzzleInputActivity extends AppCompatActivity {
         //Create queue that accepts requests
         RequestQueue queue = Volley.newRequestQueue(this);
         //Build URL and query string from JSON object
-        String url = getApplicationContext().getString(R.string.user_database_url);
+        String url = getApplicationContext().getString(R.string.answer_submission_url);
         url += '?';
-        url += "Sheet=" + "Event2" + '&';
-        url += "answer=" + answer;
+        url += "Sheet=" + "Sheet1" + '&';
+        url += "SensorID=" + QRActivity.getSensorID().substring(5,9) + '&';
+        url += "PuzzleID=" + PuzzleID + '&';
+        url += "SubmittedAnswer=" + answer;
         url = QRActivity.ensureValidURL(url);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -50,20 +61,26 @@ public class PuzzleInputActivity extends AppCompatActivity {
                         String str = response.toString();
                         Log.e("QR JSON response",str);
                         boolean result = false;
+                        String correctness = "";
                         try {
                             result = response.getString("result").equals("success");
+                            correctness = response.getString(("correct?"));
                         }
                         catch (JSONException exception) {
-                            AnswerDisplay.setText("failed");
+                            AnswerDisplay.setText(correctness);
                         }
                         if (result) {
-                            try {
-                                AnswerDisplay.setText("success");
-                            }
-                            catch (JSONException e) {
-                                AnswerDisplay.setText("failed");
-                            }
-                        }
+                            //try {
+                                AnswerDisplay.setText(correctness);
+
+                                //SharedPreferences.Editor prefEditor = userData.edit();
+                                //prefEditor.putString(GlobalParams.PUZZLEID_KEY, PuzzleID);
+                                //prefEditor.apply();
+                            //}
+                            //catch (JSONException e) {
+                            //    AnswerDisplay.setText("failed");
+                            //}
+                        }/*
                         else {
                             try {
                                 AnswerDisplay.setText("failed");
@@ -71,7 +88,7 @@ public class PuzzleInputActivity extends AppCompatActivity {
                             catch (JSONException e) {
                                 AnswerDisplay.setText("failed");
                             }
-                        }
+                        }*/
                     }
                 }, new Response.ErrorListener() {
 
